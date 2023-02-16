c9371d8f218506fb7f43d7751d857fe9f36e9caa
Johnson Adeola <johnsamxy@gmail.com>
2023-02-16 14:39:38 -0800

    corrected last added file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -5,9 +6,8 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
