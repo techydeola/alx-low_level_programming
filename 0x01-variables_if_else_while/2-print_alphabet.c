c0b00fef503e3a67c392c796fbf9eead49fd5008
Johnson Adeola <johnsamxy@gmail.com>
2023-02-17 11:51:11 -0800

    new C file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - ENtry point
+ *
+ * Always 0 success
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
+}
