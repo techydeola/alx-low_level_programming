1576a8e873d9bfd5a99ff8fa3e45e60ad362e9c6
Johnson Adeola <johnsamxy@gmail.com>
2023-02-19 05:21:17 -0800

    correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -14,7 +12,8 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-		putchar('\n');
+
+	putchar('\n');
 
 	return (0);
 }
