8ee71ef02a898ec3c3e54b453a192167cf4834cb
Johnson Adeola <johnsamxy@gmail.com>
2023-02-21 12:53:53 -0800

    new file and header declaration

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter = a;
+	int check = 0;
+
+	while (check++ <= 9)
+	{
+		for (letter = 'a'; letter = 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 /* all prototypes */
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
