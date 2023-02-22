467e611aeec5230fa5f9a896e647bd35010dd2fd
Johnson Adeola <johnsamxy@gmail.com>
2023-02-22 08:45:17 -0800

    correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,8 +14,12 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
-			_putchar("%02d:%02d\n", hour, minute);
-			return (0);
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
