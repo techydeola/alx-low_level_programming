9e454d8d38889e0063a0b59ed85d4dc61fe8486d
Johnson Adeola <johnsamxy@gmail.com>
2023-02-22 13:50:44 -0800

    new file and declaration

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds the two arguments passed in
+ * @a: the first argument
+ * @b: the second argument
+ *
+ * Return: The result of the function
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
