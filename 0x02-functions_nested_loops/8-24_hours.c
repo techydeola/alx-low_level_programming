841fa0642b2a7b91e88578f7f8db6a6de4b5987f
Johnson Adeola <johnsamxy@gmail.com>
2023-02-22 08:43:02 -0800

    new file and declaration

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar("%02d:%02d\n", hour, minute);
+			return (0);
+		}
+	}
+}
