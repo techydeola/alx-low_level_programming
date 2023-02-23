860db7b181d80806b472853ea919ea03bc51a1c9
Johnson Adeola <johnsamxy@gmail.com>
2023-02-23 11:28:10 -0800

    correction

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void);	
+void print_numbers(void);
+int _putchar(char c);
+#endif /* MAIN_H */
