666f79761f87003f0b987ccd4a2edb0de9e0460c
Johnson Adeola <johnsamxy@gmail.com>
2023-02-27 13:23:04 -0800

    new file and declaration

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value it points to
+ * @n: the argument to be checked
+ */
+
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = n;
+	*p = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains Alx projects on pointers and arrays
