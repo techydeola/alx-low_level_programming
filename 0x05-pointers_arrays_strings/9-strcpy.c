f5fb13c8704e4be9f820047489a4899432354293
Johnson Adeola <johnsamxy@gmail.com>
2023-02-28 07:30:22 -0800

    correction

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,15 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	char *start = dest;
 
-	while (src[index])
+	while (*src != '\0')
 	{
-		dest[index] = src[index];
-		index++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
+	*dest = '\0';
 
-	return (dest);
+	return (start);
 }
