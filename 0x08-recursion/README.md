f805eefb247f757995ff30195b65f24d5d6ce0ee
Johnson Adeola <johnsamxy@gmail.com>
2023-03-09 01:17:14 -0800

    new files

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This directory contains programs on C recursion
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+int _putchar(char c);
+void _puts_recursion(char *s);
+#endif /* MAIN_H */
