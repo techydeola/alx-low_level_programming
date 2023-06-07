d8f67b97a5be209c41e7a37275c9b48a2f898e03
Johnson Adeola <johnsamxy@gmail.com>
2023-06-07 04:10:57 -0700

    second commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
+#include "main.h"
 
+/**
+ * _puts_recursion - a function that prints a string
+ * @s: a pointer to the string
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
 
