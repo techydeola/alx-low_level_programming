ba8e200d5949913b8c041f6b7758821daaec00a9
Johnson adeola <johnsamxy@gmail.com>
2023-09-08 17:35:40 +0100

    quick fix

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,32 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - prints the keys and values of the hash table
+ * hash_table_delete - deletes a hash table
  *
- * @ht: a pointer to the hash table
- * Return: nothing
+ * @ht: pointer to the hash table
+ * Return: no return
  */
-void hash_table_print(const hash_table_t *ht)
+void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *tmp;
-	char *sep;
+	hash_node_t *tmp1;
+	hash_node_t *tmp2;
 
 	if (ht == NULL)
 		return;
 
-	printf("{");
-	sep = "";
-
 	for (i = 0; i < ht->size; i++)
 	{
-		tmp = ht->array[i];
-		while (tmp != NULL)
+		tmp1 = ht->array[i];
+		while ((tmp2 = tmp1) != NULL)
 		{
-			printf("%s'%s': '%s'", sep, tmp->key, tmp->value);
-			sep = ", ";
-			tmp = tmp->next;
+			tmp1 = tmp1->next;
+			free(tmp2->key);
+			free(tmp2->value);
+			free(tmp2);
 		}
 	}
-	printf("}\n");
+	free(ht->array);
+	free(ht);
 }
